8786d0448d59d7f34924bd55108d827ca990ea09
tategakibunko <lambda.watanabe@gmail.com>
2015-04-08 14:39:21 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# nehan-circular-example
+
+## Summary
+
+This is a tiny example of 'how to define custom tag in nehan.js'.
+
+
+## License
+
+MIT
+
+
---
+++ nehan-circular-example.js
@@ -0,0 +1,81 @@
+Nehan.setStyles({
+  "circular":{
+    display:"block",
+    background:"wheat",
+    measure:"280px",
+    extent:"280px",
+    "box-sizing":"cotent-box",
+    "border-radius":"250px",
+    margin:{after:"2em"},
+    content:function(ctx){
+      // set default content if markup is empty.
+      return ctx.getMarkup().isEmpty()? Nehan.List.fold(Nehan.List.create(12), "", function(ret, index){
+	return ret + "<div>" + (index + 1) + "時ですよ！</div>";
+      }) : "";
+    },
+    // set page-break if enough space is left.
+    onload:function(ctx){
+      var items = [];
+      var rest_extent = ctx.getRestExtent();
+      var extent = parseInt(ctx.getMarkup().getAttr("extent", "280px"), 10);
+      if(rest_extent < extent){
+	ctx.setCssAttr("break-before", "always");
+      }
+    }
+  },
+  "circular div":{
+    "line-height":"1em",
+    "padding":{start:"0.5em"},
+    color:function(ctx){
+      var parent_style = ctx.getParentStyleContext();
+      var child_index = ctx.getChildIndex();
+      var cur_hour = new Date().getHours() % 12;
+      return (child_index + 1 === cur_hour)? "red" : "black";
+    },
+    onblock:function(ctx){
+      var parent_style = ctx.box.style.parent;
+      var is_vert = ctx.isTextVertical();
+      var child_index = ctx.getChildIndex();
+      var child_count = ctx.getParentStyleContext().getChildCount();
+      var line_height = ctx.getStyleContext().getFontSize(); // line-height:"1em"
+      var parent_extent = ctx.getParentBox().getContentExtent();
+      var trans_extent = Math.floor((parent_extent - line_height) / 2);
+      var unit_degree = Math.floor(360 / child_count);
+      var rotate_degree = child_index * unit_degree + (is_vert? 30 : 120);
+      var $dom = $(ctx.dom);
+      var translate = is_vert? {translateX:trans_extent + "px"} : {translateY:trans_extent + "px"};
+      var rotate = {
+	opacity:1,
+	rotateZ:rotate_degree + "deg"
+      };
+
+      $dom
+	.css("position", "absolute")
+	.css("opacity", 0)
+	.velocity(translate)
+	.velocity(rotate);
+    }
+  }
+});
+
+
+$(function(){
+  $("button.demo").click(function(){
+    var $button = $(this);
+    var flow = $button.data("flow");
+    var target = $button.data("target");
+    var $target = $("#" + target);
+    var $pe = Nehan.createPagedElement({
+      config:{root:"body"} // <!doctype> and <html> are not included in content-text.
+    }).setStyle("body", {
+      "flow":flow,
+      "width":500,
+      "height":((flow === "tb-rl")? 320 : 500)
+    }).setContent($target.html());
+    $("#result-" + target).remove(); // remove previous result if exists
+    var $element = $pe.getElement();
+    $element.id = "result-" + target;
+    $target.after($element);
+  });
+});
+
